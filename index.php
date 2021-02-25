71af3125badea1f1afc7d0bc16716a15123f695a
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-02-25 22:56:09 +0700

    css js

--- index.php
+++ index.php
@@ -1,13 +1,53 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Pendaftaran Siswa Baru | SMK Coding</title>
+	<title>Penerimaan Mahasiswa baru Politehnik Negeri sriwijaya</title>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/index.css">
+    <script src="assets/jquery/jquery-3.5.1.min.js"></script>
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+  	<img src="assets/img/polsri.png" width="50" height="50" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="profil.php">Profil Mahasiswa Baru</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Berita</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Jurusan</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Akademik</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Panduan</a>
+				</li>
+            </ul>
+        </div>
+</nav>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
+	<div class="container">
 	<header>
-		<h3>Pendaftaran Siswa Baru</h3>
-		<h1>SMK Coding</h1>
+		<h3>Pendaftaran Mahasiswa Baru</h3>
+		<h1>Politehnik Negeri Sriwijaya</h1>
 	</header>
 	
 	<h4>Menu</h4>
@@ -17,13 +57,12 @@
 			<li><a href="list-siswa.php">Pendaftar</a></li>
 		</ul>
 	</nav>
-	
-	
+	</div>
 	<?php if(isset($_GET['status'])): ?>
 	<p>
 		<?php
 			if($_GET['status'] == 'sukses'){
-				echo "Pendaftaran siswa baru berhasil!";
+				echo "Pendaftaran Mahasisiswa baru berhasil!";
 			} else {
 				echo "Pendaftaran gagal!";
 			}
